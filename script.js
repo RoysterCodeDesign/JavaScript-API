bb2982101f774379789d7d5efbffeb85071a01aa
Monica R <mroy.dev@outlook.com>
2021-09-20 21:44:14 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+const api_key = "https://api.nasa.gov/planetary/apod?api_key=juMYsXjg7uuldkn779ElyHsnm3r9GeKj1G5ThdMq"
+
+const fetchNASAData = async () => {
+    try {
+        const response = await fetch(`${api_key}`)
+        const data = await response.json()
+        console.log('SPACESTAGRAM DATA', data)
+        displayData(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const displayData = data => {
+    document.getElementById('copyright').textContent = data.copyright
+    document.getElementById('title').textContent = data.title
+    document.getElementById('date').textContent = data.date
+    document.getElementById('picture').src = data.hdurl
+    document.getElementById('explanation').textContent = data.explanation
+}
+
+fetchNASAData()
+
